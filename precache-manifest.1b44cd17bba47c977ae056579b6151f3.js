d3e8c6f4f50eee92dd47b72df6b7d04b0861ba54
Geordie Powers <geordie.powers@gmail.com>
2020-02-16 23:08:03 -0500

    620366a

---
+++ precache-manifest.1b44cd17bba47c977ae056579b6151f3.js
@@ -0,0 +1,142 @@
+self.__precacheManifest = [
+  {
+    "revision": "0206793b41cdb45eceda",
+    "url": "/static/js/0.0206793b.chunk.js"
+  },
+  {
+    "revision": "c79bbdec945ed5fbff42",
+    "url": "/static/css/main.7e2b7afe.chunk.css"
+  },
+  {
+    "revision": "c79bbdec945ed5fbff42",
+    "url": "/static/js/main.c79bbdec.chunk.js"
+  },
+  {
+    "revision": "62eb30004b397edbd692",
+    "url": "/static/css/2.31d49b7e.chunk.css"
+  },
+  {
+    "revision": "62eb30004b397edbd692",
+    "url": "/static/js/2.62eb3000.chunk.js"
+  },
+  {
+    "revision": "c7af55675e5765a79faf",
+    "url": "/static/js/3.c7af5567.chunk.js"
+  },
+  {
+    "revision": "8dd27aae8e15ad7fcd18",
+    "url": "/static/js/4.8dd27aae.chunk.js"
+  },
+  {
+    "revision": "4600a1b2f548af6bd8fe",
+    "url": "/static/js/5.4600a1b2.chunk.js"
+  },
+  {
+    "revision": "990c5257988aae36b3a7",
+    "url": "/static/css/6.1824b1f2.chunk.css"
+  },
+  {
+    "revision": "990c5257988aae36b3a7",
+    "url": "/static/js/6.990c5257.chunk.js"
+  },
+  {
+    "revision": "30ea74c0894bdc956b6c",
+    "url": "/static/js/7.30ea74c0.chunk.js"
+  },
+  {
+    "revision": "7512e075417456f0f506",
+    "url": "/static/js/8.7512e075.chunk.js"
+  },
+  {
+    "revision": "ebbe1e986af5d2dc96ba",
+    "url": "/static/js/9.ebbe1e98.chunk.js"
+  },
+  {
+    "revision": "abf278066ec9f52d35f6",
+    "url": "/static/js/10.abf27806.chunk.js"
+  },
+  {
+    "revision": "0908009d291ebbe7f4f8",
+    "url": "/static/js/11.0908009d.chunk.js"
+  },
+  {
+    "revision": "3054338bea611e2d4204",
+    "url": "/static/js/12.3054338b.chunk.js"
+  },
+  {
+    "revision": "d124050a7b7034679f47",
+    "url": "/static/js/13.d124050a.chunk.js"
+  },
+  {
+    "revision": "25c1c5609ddd2577792c",
+    "url": "/static/js/14.25c1c560.chunk.js"
+  },
+  {
+    "revision": "bec5874a050944edcab1",
+    "url": "/static/js/15.bec5874a.chunk.js"
+  },
+  {
+    "revision": "685cfdbc73c3d9a9c372",
+    "url": "/static/js/16.685cfdbc.chunk.js"
+  },
+  {
+    "revision": "e9e4be7b77301b614b0a",
+    "url": "/static/js/runtime~main.e9e4be7b.js"
+  },
+  {
+    "revision": "f9f0eba5e64bc10b381858d8094ef68b",
+    "url": "/static/media/shopping-mocks.f9f0eba5.png"
+  },
+  {
+    "revision": "33974ce24b0c45274f8b01914bccfe82",
+    "url": "/static/media/issue-tracker.33974ce2.png"
+  },
+  {
+    "revision": "1d0f850911293410d1ba5786d60b9d21",
+    "url": "/static/media/twitchi-home.1d0f8509.png"
+  },
+  {
+    "revision": "52414266d539991b326631d32b3b147e",
+    "url": "/static/media/twitchi-icon.52414266.png"
+  },
+  {
+    "revision": "4678d3376c8539d1f413df8872455f32",
+    "url": "/static/media/FFMetaVariable.4678d337.ttf"
+  },
+  {
+    "revision": "629a55a7e793da068dc580d184cc0e31",
+    "url": "/static/media/OpenSans-Regular.629a55a7.ttf"
+  },
+  {
+    "revision": "50145685042b4df07a1fd19957275b81",
+    "url": "/static/media/OpenSans-Bold.50145685.ttf"
+  },
+  {
+    "revision": "8710d574cab29104c9dcb19bd85bc241",
+    "url": "/static/media/SwiftShop_wk3_diagram.8710d574.svg"
+  },
+  {
+    "revision": "e2a11462c15729fce1eb0c0d91145ac1",
+    "url": "/static/media/SwiftShop_wk6_err1.e2a11462.png"
+  },
+  {
+    "revision": "bad169f9e9f737f4c3c99d1786c633df",
+    "url": "/static/media/SwiftShop_wk6_err2.bad169f9.png"
+  },
+  {
+    "revision": "cf5bba546cb2d1459a081a2fd8c765ef",
+    "url": "/static/media/gp-infinity.cf5bba54.svg"
+  },
+  {
+    "revision": "5b958cad1252faab588aba138305971c",
+    "url": "/static/media/wave.5b958cad.svg"
+  },
+  {
+    "revision": "b1824976cfbcffb174b2f1d2d175fcaf",
+    "url": "/static/media/gp-infinity-gradient.b1824976.svg"
+  },
+  {
+    "revision": "40b3b46e4de024eef4c651f395892793",
+    "url": "/index.html"
+  }
+];
